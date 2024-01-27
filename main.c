81723df9197a13dcf27cabc91ac1a1ef809a2463
roni-naftalovich <157619305+roni-naftalovich@users.noreply.github.com>
2024-01-27 14:30:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,38 +13,38 @@ int main() {
        x1=x2;
        x2=temp;
    }
-   printf("The Armstrong numbers are:");
+   printf("The Armstrong numbers are: ");
    for(int i=x1;i<=x2;i++)
    {
        if(isArmstrong(i)==1){
-           printf(" %d",i);
+           printf("%d ",i);
        }
    }
    printf("\n");
-    printf("The Palindromes are:");
+    printf("The Palindromes are: ");
     for(int i=x1;i<=x2;i++)
    {
        if(isPalindrome(i)==1){
-           printf(" %d",i);
+           printf("%d ",i);
        }
    }
    printf("\n");
-   printf("The Prime numbers are:");
+   printf("The Prime numbers are: ");
 
    for(int i=x1;i<=x2;i++)
    {
        if(isPrime(i)==1){
-           printf(" %d",i);
+           printf("%d ",i);
        }
    }
    printf("\n");
     
-      printf("The Strong numbers are:");
+      printf("The Strong numbers are: ");
 
     for(int i=x1;i<=x2;i++)
    {
        if(isStrong(i)==1){
-           printf(" %d",i);
+           printf("%d ",i);
        }
    }
     
